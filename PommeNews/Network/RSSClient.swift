a7751460db327a40b4bd37b2fce00a222d6c5f42
Jonathan Duss <jonathan.duss@iml.unibe.ch>
2018-07-11 23:00:37 +0200

    RssClient improvement

--- PommeNews/Network/RSSClient.swift
+++ PommeNews/Network/RSSClient.swift
@@ -33,61 +33,72 @@ class RSSClient {
         
         
         let parser = FeedParser(URL: feed.url)
+        
+        
         parser?.parseAsync(result: { result in
             
             var articles: [RssArticlePO] = []
-
+            
             switch result {
             case .atom(_):
+                completion(Result.failure(PError.unsupported))
                 assertionFailure("Atom not supported")
-//                guard let entries = atomFeed.entries else { return }
-//
-//                var articles: [RssArticle] = []
-//
-//                for entry in entries {
-//
-//                    guard let title = entry.title,
-//                        let summary = entry.summary?.value,
-//                        let link = entry.links?.first,
-//                        let date = entry.published
-//                        else {
-//                            continue
-//                    }
-//
-//                    let article = RssArticle(title: title,
-//                                             summary: summary,
-//                                             feed: RSSFeed.atom(atomFeed),
-//                                             imageURL: URL(string: entry.media?.mediaThumbnails?.first?.attributes?.url ?? ""),
-//                                             date: date,
-//                                             link: URL(string: link.attributes?.href ?? ""),
-//                                             creator: entry.authors.first.name ?? "rssarticle.unknownAuthor".localized)
-//                    articles.append(article)
-//                }
-//                break
+                //                guard let entries = atomFeed.entries else { return }
+                //
+                //                var articles: [RssArticle] = []
+                //
+                //                for entry in entries {
+                //
+                //                    guard let title = entry.title,
+                //                        let summary = entry.summary?.value,
+                //                        let link = entry.links?.first,
+                //                        let date = entry.published
+                //                        else {
+                //                            continue
+                //                    }
+                //
+                //                    let article = RssArticle(title: title,
+                //                                             summary: summary,
+                //                                             feed: RSSFeed.atom(atomFeed),
+                //                                             imageURL: URL(string: entry.media?.mediaThumbnails?.first?.attributes?.url ?? ""),
+                //                                             date: date,
+                //                                             link: URL(string: link.attributes?.href ?? ""),
+                //                                             creator: entry.authors.first.name ?? "rssarticle.unknownAuthor".localized)
+                //                    articles.append(article)
+                //                }
+                //                break
                 
             case .rss(let rssFeed):
                 guard let entries = rssFeed.items else { return }
+                
+                print(feed.url)
+                
+                
                 for entry in entries {
                     
                     guard let title = entry.title,
                         let summary = entry.description,
-                        let creator = entry.author,
                         let date = entry.pubDate
                         else {
                             continue
                     }
                     
                     let article = RssArticlePO(titleHtml: title,
-                                             summaryHtml: summary.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines),
-                                             imageUrl: URL(string: entry.media?.mediaThumbnails?.first?.attributes?.url),
-                                             date: date,
-                                             link: URL(string: entry.link),
-                                             creator: creator)
+                                               summaryHtml: summary.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines),
+                                               imageUrl: URL(string: entry.media?.mediaThumbnails?.first?.attributes?.url),
+                                               date: date,
+                                               link: URL(string: entry.link),
+                                               creator: entry.author ?? "?")
                     articles.append(article)
-                    completion(Result.success(articles))
                 }
+                
+                completion(Result.success(articles))
+                
+                
                 break
             case .json(_):
+                completion(Result.failure(PError.unsupported))
+                
                 assertionFailure("Json not supported")
                 break
             case .failure(let error):
