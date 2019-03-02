a382daf5ee62d5a45521d399eedb2a9268de0368
Jonathan Duss <jonathan.duss@iml.unibe.ch>
2019-03-02 17:26:01 +0100

    Fixed project config to be uploaded successfully to Testflight
    Former-commit-id: 60712163d9d070263ca5541556776e8ae8092dea

---
+++ PommeNews/PommeNews/Settings/ApplicationSettings.swift
@@ -0,0 +1,9 @@
+//
+//  ApplicationSettings.swift
+//  PommeNews
+//
+//  Created by Jonathan Duss on 02.03.19.
+//  Copyright © 2019 Swizapp. All rights reserved.
+//
+
+import Foundation
---
+++ PommeNews/PommeNews/UI/ApplicationInfo/PrivacyPolicyVC.swift
@@ -0,0 +1,30 @@
+//
+//  PrivacyPolicyVC.swift
+//  PommeNews
+//
+//  Created by Jonathan Duss on 02.03.19.
+//  Copyright © 2019 Swizapp. All rights reserved.
+//
+
+import UIKit
+
+class PrivacyPolicyVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
