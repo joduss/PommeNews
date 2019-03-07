444ce2fcfcfa064863aecaafa46e2343992f848f
Jonathan Duss <jonathan.duss@iml.unibe.ch>
2019-03-07 22:50:46 +0100

    Created a tool to measure time performance
    Former-commit-id: 45c190a0b0b1c42dde1cdd8dfb3d044547a70486

--- ZaJoLibrary/ZaJoLibrary/Performance.swift
+++ ZaJoLibrary/ZaJoLibrary/Performance.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+public class Performance {
+    
+    public static func measure(title: String = "Operation", code: () -> ()) {
+        let start = Date()
+        code()
+        let stop = Date()
+        
+        let seconds = stop.timeIntervalSinceReferenceDate - start.timeIntervalSinceReferenceDate
+        
+        if seconds < pow(10,-6) {
+            NSLog("\(title) took \(seconds / pow(10,-9)) nanoseconds")
+        }
+        else if seconds < 100 {
+            NSLog("\(title) took \(seconds / pow(10,-3)) milliseconds")
+        }
+        else {
+            NSLog("\(title) took \(seconds) seconds")
+        }
+    }
+    
+}
