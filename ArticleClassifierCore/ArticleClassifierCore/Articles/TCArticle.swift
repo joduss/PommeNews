b24bcf54a13e9e457b521fb9f566831aac8baeaf
Jonathan Duss <jonathan.duss@iml.unibe.ch>
2019-03-30 12:34:08 +0100

    Prepared the TCArticle for similar article finding
    Former-commit-id: 6bdaf07bfef7915551eb631d1ee051972098d8d3

--- ArticleClassifierCore/ArticleClassifierCore/Articles/TCArticle.swift
+++ ArticleClassifierCore/ArticleClassifierCore/Articles/TCArticle.swift
@@ -10,16 +10,40 @@ import Foundation
 
 
 public struct TCArticle: Codable, Hashable {
+
     public let title: String
     public let summary: String
     public let themes: [String]
+    public let associatedObject: AnyHashable?
     
-    public init(title: String?, summary: String?, themes: [String] = []) {
+    private enum CodingKeys: String, CodingKey {
+        case title
+        case summary
+        case themes
+    }
+    
+    public init(title: String?, summary: String?, themes: [String] = [], associatedObject: AnyHashable? = nil) {
         self.title = title ?? ""
         self.summary = summary ?? ""
         self.themes = themes
+        self.associatedObject = associatedObject
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        title = try values.decode(String.self, forKey: .title)
+        summary = try values.decode(String.self, forKey: .summary)
+        themes = try values.decode([String].self, forKey: .themes)
+        associatedObject = nil
     }
     
     
+    public static func == (lhs: TCArticle, rhs: TCArticle) -> Bool {
+        return (lhs.title + lhs.summary).hashValue == (rhs.title + rhs.summary).hashValue
+    }
+    
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(title + summary)
+    }
 
 }
