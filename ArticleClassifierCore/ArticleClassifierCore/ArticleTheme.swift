a5be275db716c0f720411dcd68d897460235762f
Jonathan Duss <jonathan.duss@iml.unibe.ch>
2020-01-05 17:12:28 +0100

    Classifier: Added more themes
    Former-commit-id: 36c90ed8c191bb72bf270cc302dc5b53081dd5fc

--- ArticleClassifierCore/ArticleClassifierCore/ArticleTheme.swift
+++ ArticleClassifierCore/ArticleClassifierCore/ArticleTheme.swift
@@ -18,13 +18,20 @@ public struct ArticleTheme: Hashable, Equatable {
     public static let mac = ArticleTheme(key: "mac")
     public static let otherAppleProduct = ArticleTheme(key:"otherAppleProduct")
     public static let pc = ArticleTheme(key: "pc")
+    public static let otherPhone = ArticleTheme(key: "otherPhone")
+    public static let otherTablet = ArticleTheme(key: "otherTablet")
+    public static let otherWear = ArticleTheme(key: "otherWear")
+    public static let androidPhone = ArticleTheme(key: "androidPhone")
+    public static let androidTablet = ArticleTheme(key: "androidTablet")
+    public static let androidWear = ArticleTheme(key: "androidWear")
     public static let surface = ArticleTheme(key: "surface")
-    
+
     public static let android = ArticleTheme(key: "android")
     public static let ios = ArticleTheme(key: "ios")
     public static let windows = ArticleTheme(key: "windows")
     public static let macos = ArticleTheme(key: "macos")
-    
+    public static let otherOS = ArticleTheme(key: "otherOS")
+
     public static let apps = ArticleTheme(key: "apps")
     public static let game = ArticleTheme(key: "game")
 
@@ -33,6 +40,9 @@ public struct ArticleTheme: Hashable, Equatable {
     public static let tablet = ArticleTheme(key: "tablet")
     public static let watch = ArticleTheme(key: "watch")
     public static let otherProduct = ArticleTheme(key: "otherProduct")
+    public static let smartCar = ArticleTheme(key: "smartCar")
+    public static let smartHome = ArticleTheme(key: "smartHome")
+    public static let speaker = ArticleTheme(key: "speaker")
 
     public static let apple = ArticleTheme(key: "apple")
     public static let microsoft = ArticleTheme(key: "microsoft")
@@ -42,29 +52,50 @@ public struct ArticleTheme: Hashable, Equatable {
     public static let spotify = ArticleTheme(key: "spotify")
     public static let netflix = ArticleTheme(key: "netflix")
     public static let facebook = ArticleTheme(key: "facebook")
+    public static let amazonPrime = ArticleTheme(key: "amazonPrime")
+    public static let hbo = ArticleTheme(key: "hbo")
+    public static let disneyPlus = ArticleTheme(key: "disneyPlus")
 
     
     public static let appleMusic = ArticleTheme(key: "appleMusic")
     public static let appleTVplus = ArticleTheme(key: "appleTVplus")
     public static let appleNews = ArticleTheme(key: "appleNews")
+    public static let applePay = ArticleTheme(key: "applePay")
     public static let icloud = ArticleTheme(key: "icloud")
     public static let appleServices = ArticleTheme(key: "appleServices")
+    public static let services = ArticleTheme(key: "services")
+    public static let paymentService = ArticleTheme(key: "paymentService")
+    public static let cloudService = ArticleTheme(key: "cloudService")
 
+    public static let audioVisualCulture = ArticleTheme(key: "photo")
     
-    public static let music = ArticleTheme(key: "music")
-    public static let video = ArticleTheme(key: "video")
-    public static let photo = ArticleTheme(key: "photo")
+    public static let audioDeviceTech = ArticleTheme(key: "musicDeviceTech")
+    public static let videoDeviceTech = ArticleTheme(key: "videoDeviceTech")
+    public static let photoDeviceTech = ArticleTheme(key: "photoDeviceTech")
+    
+    public static let videoService = ArticleTheme(key: "videoService")
+    public static let audioService = ArticleTheme(key: "audioService")
 
 
     public static let promo = ArticleTheme(key: "promo")
+    public static let advertisement = ArticleTheme(key: "advertisement")
     public static let rumor = ArticleTheme(key: "rumor")
     public static let security = ArticleTheme(key: "security")
     public static let privacy = ArticleTheme(key: "privacy")
     public static let patent = ArticleTheme(key: "patent")
+    public static let lawsuitLegal = ArticleTheme(key: "lawsuitLegal")
     public static let technology = ArticleTheme(key: "technology")
     public static let beta = ArticleTheme(key: "beta")
     public static let economyPolitic = ArticleTheme(key: "economyPolitic")
     public static let test = ArticleTheme(key: "test")
+    public static let future = ArticleTheme(key: "future")
+    public static let IT = ArticleTheme(key: "IT")
+    public static let AIML = ArticleTheme(key: "AIML")
+    public static let networkProvider = ArticleTheme(key: "networkProvider")
+    public static let study = ArticleTheme(key: "study")
+    public static let crypto = ArticleTheme(key: "crypto")
+    public static let health = ArticleTheme(key: "health")
+    public static let cloudComputing = ArticleTheme(key: "cloudComputing")
 
     public static let accessory = ArticleTheme(key: "accessory")
     public static let component = ArticleTheme(key: "component")
@@ -81,14 +112,22 @@ public struct ArticleTheme: Hashable, Equatable {
         ArticleTheme.appleWatch,
         ArticleTheme.mac,
         ArticleTheme.otherAppleProduct,
+        
         ArticleTheme.pc,
         ArticleTheme.surface,
+        ArticleTheme.androidPhone,
+        ArticleTheme.androidTablet,
+        ArticleTheme.androidWear,
+        ArticleTheme.otherPhone,
+        ArticleTheme.otherTablet,
+        ArticleTheme.otherWear,
         
         ArticleTheme.android,
         ArticleTheme.ios,
         ArticleTheme.macos,
         ArticleTheme.windows,
-        
+        ArticleTheme.beta,
+
         ArticleTheme.apps,
         ArticleTheme.game,
 
@@ -97,6 +136,9 @@ public struct ArticleTheme: Hashable, Equatable {
         ArticleTheme.tablet,
         ArticleTheme.watch,
         ArticleTheme.otherProduct,
+        ArticleTheme.smartCar,
+        ArticleTheme.smartHome,
+        ArticleTheme.speaker,
         
         ArticleTheme.amazon,
         ArticleTheme.apple,
@@ -104,33 +146,53 @@ public struct ArticleTheme: Hashable, Equatable {
         ArticleTheme.google,
         ArticleTheme.microsoft,
         ArticleTheme.netflix,
+        ArticleTheme.hbo,
+        ArticleTheme.amazonPrime,
+        ArticleTheme.disneyPlus,
         ArticleTheme.samsung,
         ArticleTheme.spotify,
         
         ArticleTheme.appleMusic,
         ArticleTheme.appleNews,
         ArticleTheme.appleTVplus,
+        ArticleTheme.applePay,
         ArticleTheme.icloud,
         ArticleTheme.appleServices,
+        ArticleTheme.paymentService,
+        ArticleTheme.services,
+        ArticleTheme.cloudService,
+
+        ArticleTheme.audioVisualCulture, //
         
-        ArticleTheme.video,
-        ArticleTheme.photo,
-        ArticleTheme.music,
+        ArticleTheme.videoDeviceTech, // AppleTV, TV, tech for video, apps of edition
+        ArticleTheme.audioDeviceTech, // Speaker, tech for audio, apps of edition
+        ArticleTheme.photoDeviceTech, // Test of camera, camera tech, sensor, etc, apps of edition
+
+        ArticleTheme.audioService, //deezer, spotify, etc streaming
+        ArticleTheme.videoService, //netflix, prime, etc
 
         
         ArticleTheme.promo,
+        ArticleTheme.advertisement,
         ArticleTheme.rumor,
         ArticleTheme.security,
         ArticleTheme.privacy,
         ArticleTheme.technology,
         ArticleTheme.patent,
+        ArticleTheme.lawsuitLegal,
         ArticleTheme.economyPolitic,
-        ArticleTheme.test,
-        ArticleTheme.beta,
+        ArticleTheme.test, // Tests or comparison
+        ArticleTheme.health,
+        ArticleTheme.crypto,
+        ArticleTheme.AIML, // Machine Learning et cie
+        ArticleTheme.IT, // Other IT news
+        ArticleTheme.study,
+        ArticleTheme.networkProvider,
+        ArticleTheme.future,
+        ArticleTheme.cloudComputing,
 
-
-        ArticleTheme.component,
-        ArticleTheme.accessory,
+        ArticleTheme.component, // Any bind of component for tech (ssd, )
+        ArticleTheme.accessory, // Accessory for devices (usb cable, color, etc)
         ArticleTheme.keynote,
         
         ArticleTheme.otherSerious,
