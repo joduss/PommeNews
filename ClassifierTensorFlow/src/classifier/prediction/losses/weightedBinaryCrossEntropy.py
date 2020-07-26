cae940de3c27acb68f986ff3c489fab64ebbf9e1
Jonathan Duss <jonathan.duss@iml.unibe.ch>
2020-07-26 18:44:31 +0200

    Classifier: Improved WeightedBinaryCrossentropy function
    Former-commit-id: 53a90089ee5ae63d77c09770778b7e8b9292b284

--- ClassifierTensorFlow/src/classifier/prediction/losses/weightedBinaryCrossEntropy.py
+++ ClassifierTensorFlow/src/classifier/prediction/losses/weightedBinaryCrossEntropy.py
@@ -1,49 +1,24 @@
 from typing import List
-import tensorflow as tf
 
+import tensorflow as tf
+from tensorflow.keras import backend as K
 from tensorflow.python.keras.losses import BinaryCrossentropy
 
 
 class WeightedBinaryCrossEntropy(BinaryCrossentropy):
+    """
+    Implementation follows https://stackoverflow.com/questions/48485870/multi-label-classification-with-class-weights-in-keras
+    """
 
-    def __init__(self, weights: List[float], from_logits: bool):
-        super().__init__(from_logits=from_logits)
-        self.weights = weights
+
+    def __init__(self, weights: List[List[float]]):
+        super().__init__()
+        self.weights = tf.constant(weights)
+
 
     def call(self, y_true, y_pred):
-
-        nb_pred = tf.shape(y_true)[0]
-
-        epsilon = 0
-
-
-        weight_tensor = tf.convert_to_tensor([self.weights])
-        weight_tensor = 1 / weight_tensor
-
-        penalization_matrix = tf.tile(weight_tensor, tf.convert_to_tensor([nb_pred,1]) )
-        #penalization_matrix = tf.tile(weight_tensor, tf.constant([nb_pred,1]) )
-        penalization_matrix = penalization_matrix * -(y_true - 1) + y_true
-
-        inline_loss = - (1 - y_true) * self.log2(1 - y_pred + epsilon) - y_true * self.log2(y_pred - epsilon)
-        #inline_loss = tf.math.maximum(y_true, 0) - y_true * y_pred + tf.math.log(1 + tf.math.exp(-tf.math.abs(y_true)))
-
-        weighted_loss = inline_loss * penalization_matrix
-
-        # y_true[0:10, 1] * 2
-
-        # dtype.
-
-
-        #loss = tf.reduce_sum(weighted_loss)
-
-        #penalization_matrix = tf.tile(weight_tensor, tf.convert_to_tensor([nb_pred,1]) )
-
-        #weighted_loss = tf.where(tf.math.is_nan(weighted_loss), x=[0], y=weighted_loss)
-
-        return tf.reduce_sum(weighted_loss, 1)
-
-
-    def log2(self, x):
-        numerator = tf.math.log(x)
-        denominator = tf.math.log(tf.constant(2, dtype=numerator.dtype))
-        return numerator / denominator
+        return \
+            K.mean(
+                (self.weights[:, 0] ** (1 - y_true)) * (self.weights[:, 1] ** (y_true)) * K.binary_crossentropy(y_true,y_pred),
+                axis=-1
+            )
