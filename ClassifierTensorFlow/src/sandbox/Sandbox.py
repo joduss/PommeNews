b0e7aba005e8796bde0fda8942249f3b861a733c
Jonathan Duss <jonathan.duss@iml.unibe.ch>
2020-08-04 20:55:19 +0200

    Classifier: Playing with the 'sandbox'
    Former-commit-id: 184f6f903f1a7187154c90623611d458912d7c88

--- ClassifierTensorFlow/src/sandbox/Sandbox.py
+++ ClassifierTensorFlow/src/sandbox/Sandbox.py
@@ -1,21 +1,65 @@
 import subprocess
 import tempfile
+from math import ceil
 from shlex import shlex
 from subprocess import call
 import os
+import tensorflow as tf
 
-# print(subprocess.check_output(['/Users/jonathanduss/Desktop/ArticlePreprocessorTool']), os.getcwd())
+# print(subprocess.check_output(['/Users/jonathanduss/Desktop/ArticlePreprocessorTool2']), os.getcwd())
+#
+# (file, path) = tempfile.mkstemp()
+# print(file)
+# print(path)
+#
+# process = subprocess.Popen(["./ArticlePreprocessorTool", '/Users/jonathanduss/Desktop/untitled folder/articles_fr_28.json', '/Users/jonathanduss/Desktop/untitled folder/articles_fr_28_out.json'], stdout=subprocess.PIPE)
+# #process = subprocess.Popen(["sh", 'a.sh'], stdout=subprocess.PIPE)
+#
+#
+# while True:
+#     p = process.poll()
+#     output = process.stdout.readline()
+#     if process.poll() is not None:
+#         break
+#     if output:
+#         print(output.strip())
 
-(file, path) = tempfile.mkstemp()
-print(file)
-print(path)
+def iterate(dataset: tf.data.Dataset, batch_count):
+    for batch in dataset:
+        print(batch.numpy())
 
-process = subprocess.Popen(["./ArticlePreprocessorTool", '/Users/jonathanduss/Desktop/untitled folder/articles_fr_28.json', '/Users/jonathanduss/Desktop/untitled folder/articles_fr_28_out.json'], stdout=subprocess.PIPE)
-while True:
-    p = process.poll()
-    output = process.stdout.readline()
-    if process.poll() is not None:
-        break
-    if output:
-        print(output.strip())
+    #  for i in range(0,batch_count):
+    # #     print(list(dataset.as_numpy_iterator()))
+    #     iterator = dataset.__iter__()
+    #     try:
+    #         value = iterator.get_next()
+    #         print(value)
+    #     except tf.errors.OutOfRangeError:
+    #         pass
 
+x = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+d = tf.data.Dataset.from_tensor_slices((x)).shuffle(15)
+
+d1_batch_size = 3
+d1_batch_count = ceil(10 / d1_batch_size)
+d2_batch_size = 4
+d2_batch_count = ceil(10 / d2_batch_size)
+
+d1 = d.take(10).shuffle(15).batch(d1_batch_size)
+
+d2 = d.skip(10).take(5).batch(d2_batch_size)
+
+print(d1)
+print(d2)
+
+print("Iterate on d1")
+iterate(d1, d1_batch_count)
+
+print("Iterate second time on d1")
+iterate(d1, d1_batch_count)
+
+
+
+print("Iterate d2")
+iterate(d2, d2_batch_count)
