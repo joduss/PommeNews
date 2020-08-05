ab0f029bf56fe4bef03311dcf27034e57ef8a16b
Jonathan Duss <jonathan.duss@iml.unibe.ch>
2020-08-05 23:43:57 +0200

    Classifier: Protect Article by allowing to make it immutable.
    Former-commit-id: a07049766841c032dc293657a263a79409e40932

--- ClassifierTensorFlow/src/data_models/article.py
+++ ClassifierTensorFlow/src/data_models/article.py
@@ -1,15 +1,17 @@
-from dataclasses import dataclass
 from typing import List, Dict
 
 
 class Article:
-    id: str
-    title: str
-    summary: str
 
-    themes: List[str]
-    predicted_themes: List[str]
-    verified_themes: List[str]
+    __immutable: bool = False
+
+    __id: str
+    __title: str
+    __summary: str
+
+    __themes: List[str]
+    __predicted_themes: List[str]
+    __verified_themes: List[str]
 
 
     def __init__(self, id: str, title: str, summary: str, themes: List[str], predicted_themes: List[str],
@@ -56,3 +58,57 @@ class Article:
                and self.themes == other_article.themes \
                and self.verified_themes == other_article.verified_themes \
                and self.predicted_themes == other_article.predicted_themes
+
+    def make_immutable(self):
+        self.__immutable = True
+
+
+    @property
+    def id(self):
+        return self.__id
+
+    @id.setter
+    def id(self, id):
+        self.__raise_if_immutable()
+        self.__id = id
+
+    @property
+    def title(self):
+        return self.__title
+
+    @title.setter
+    def title(self, value):
+        self.__raise_if_immutable()
+        self.__title = value
+
+    @property
+    def summary(self):
+        return self.__summary
+
+    @summary.setter
+    def summary(self, value):
+        self.__raise_if_immutable()
+        self.__summary = value
+
+    @property
+    def themes(self):
+        return self.__themes
+
+    @themes.setter
+    def themes(self, value):
+        self.__raise_if_immutable()
+        self.__themes = value
+
+    @property
+    def verified_themes(self):
+        return self.__verified_themes
+
+    @verified_themes.setter
+    def verified_themes(self, value):
+        self.__raise_if_immutable()
+        self.__verified_themes = value
+
+
+    def __raise_if_immutable(self):
+        if self.__immutable:
+            raise Exception("This article has been made immutable!")
