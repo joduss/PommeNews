2ad6ef4a12450be0158703343de52ef3bb273ad3
Jonathan Duss <jonathan.duss@iml.unibe.ch>
2020-08-05 23:47:00 +0200

    Classifier: Fix plot of F1 and AUC when only one theme is used for the classification.
    Former-commit-id: 2b87d92788f4802b6c165570a57c703d6f46e523

--- ClassifierTensorFlow/src/classifier/evaluation/F1AUC/ThemeMetricF1AUCAggregator.py
+++ ClassifierTensorFlow/src/classifier/evaluation/F1AUC/ThemeMetricF1AUCAggregator.py
@@ -45,7 +45,6 @@ class ThemeMetricF1AUCAggregator(ThemeMetricAggregator):
 
         ax = fig.subplots(len(self.__supported_themes__), 2, sharex="all", sharey="all")
 
-
         i = 0
         for theme in self.__supported_themes__:
             f1_validation = self.perThemeMetricsValidation.get_f1(theme)
@@ -56,17 +55,30 @@ class ThemeMetricF1AUCAggregator(ThemeMetricAggregator):
 
             x = range(0, len(f1_train))
 
-            # if len(theme) == 1:
-            ax[i, 0].plot(x, f1_validation, label="f1-validation")
-            ax[i, 0].plot(x, f1_train, label="f1-train")
+            if len(self.__supported_themes__) == 1:
+                # Single theme
+                ax[0].plot(x, f1_validation, label="f1-validation")
+                ax[0].plot(x, f1_train, label="f1-train")
 
-            ax[i, 1].plot(x, auc_validation, label="auc-validation")
-            ax[i, 1].plot(x, auc_train, label="auc-train")
+                ax[1].plot(x, auc_validation, label="auc-validation")
+                ax[1].plot(x, auc_train, label="auc-train")
 
-            ax[i, 0].set_title(theme)
-            ax[i, 1].set_title(theme)
-            ax[i, 1].legend()
-            ax[i, 0].legend()
+                ax[0].set_title(theme)
+                ax[1].set_title(theme)
+                ax[1].legend()
+                ax[0].legend()
+            else:
+                # Multiple themes
+                ax[i, 0].plot(x, f1_validation, label="f1-validation")
+                ax[i, 0].plot(x, f1_train, label="f1-train")
+
+                ax[i, 1].plot(x, auc_validation, label="auc-validation")
+                ax[i, 1].plot(x, auc_train, label="auc-train")
+
+                ax[i, 0].set_title(theme)
+                ax[i, 1].set_title(theme)
+                ax[i, 1].legend()
+                ax[i, 0].legend()
         
             i+=1
 
