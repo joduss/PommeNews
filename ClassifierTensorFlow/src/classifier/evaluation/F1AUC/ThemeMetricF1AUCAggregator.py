7e299ef91b4dfdbec75957b835a727ce0b67fe75
Jonathan Duss <jonathan.duss@iml.unibe.ch>
2020-11-08 19:03:16 +0100

    Classifier: display precision and recall while training.
    Former-commit-id: 92ed795da1bd07a65a106005db1750695478b252

--- ClassifierTensorFlow/src/classifier/evaluation/F1AUC/ThemeMetricF1AUCAggregator.py
+++ ClassifierTensorFlow/src/classifier/evaluation/F1AUC/ThemeMetricF1AUCAggregator.py
@@ -40,13 +40,12 @@ class ThemeMetricF1AUCAggregator(ThemeMetricAggregator):
 
 
     def plot(self, block: bool = False):
-        fig: Figure = plt.figure("plot_score_per_theme")
+        fig: Figure = plt.figure("plot_score_per_theme", figsize=[15,10])
 
         plt.rcParams['axes.grid'] = True
         plt.clf()
         plt.ion()
 
-        ax = fig.subplots(len(self.__supported_themes__), 2, sharex="all", sharey="all")
 
         i = 0
         for theme in self.__supported_themes__:
@@ -56,21 +55,41 @@ class ThemeMetricF1AUCAggregator(ThemeMetricAggregator):
             auc_validation = self.perThemeMetricsValidation.get_auc(theme)
             auc_train = self.perThemeMetricsTrain.get_auc(theme)
 
+            precision_validation = self.perThemeMetricsValidation.get_precision(theme)
+            precision_train = self.perThemeMetricsTrain.get_precision(theme)
+
+            recall_validation = self.perThemeMetricsValidation.get_recall(theme)
+            recall_train = self.perThemeMetricsTrain.get_recall(theme)
+
+            ax = fig.subplots(2, 2, sharex="all", sharey="all")
             x = range(0, len(f1_train))
 
             if len(self.__supported_themes__) == 1:
                 # Single theme
-                ax[0].plot(x, f1_validation, label="f1-validation")
-                ax[0].plot(x, f1_train, label="f1-train")
+                ax[0, 0].plot(x, f1_validation, label="f1-validation")
+                ax[0, 0].plot(x, f1_train, label="f1-train")
 
-                ax[1].plot(x, auc_validation, label="auc-validation")
-                ax[1].plot(x, auc_train, label="auc-train")
+                ax[0, 1].plot(x, auc_validation, label="auc-validation")
+                ax[0, 1].plot(x, auc_train, label="auc-train")
 
-                ax[0].set_title(theme)
-                ax[1].set_title(theme)
-                ax[1].legend()
-                ax[0].legend()
+                ax[1, 0].plot(x, precision_validation, label="precision-validation")
+                ax[1, 0].plot(x, precision_train, label="precision-train")
+
+                ax[1, 1].plot(x, recall_validation, label="recall-validation")
+                ax[1, 1].plot(x, recall_train, label="recall-train")
+
+                ax[0, 0].set_title("F1 - " + theme)
+                ax[0, 1].set_title("AUC")
+                ax[1, 0].set_title("precision")
+                ax[1, 1].set_title("recall")
+
+                ax[0, 1].legend()
+                ax[0, 0].legend()
+                ax[1, 1].legend()
+                ax[1, 0].legend()
             else:
+                ax = fig.subplots(len(self.__supported_themes__), 2, sharex="all", sharey="all")
+
                 # Multiple themes
                 ax[i, 0].plot(x, f1_validation, label="f1-validation")
                 ax[i, 0].plot(x, f1_train, label="f1-train")
--- ClassifierTensorFlow/src/classifier/evaluation/F1AUC/ThemeMetricF1AUCList.py
+++ ClassifierTensorFlow/src/classifier/evaluation/F1AUC/ThemeMetricF1AUCList.py
@@ -11,21 +11,33 @@ class ThemeMetricF1AUCList:
         self.metrics = {}
 
     def add(self, theme: str, metric: MetricF1AUC):
-
         if theme not in self.metrics:
             self.metrics[theme] = []
 
         self.metrics[theme].append(metric)
 
     def get_f1(self, theme: str) -> List[float]:
-
         if theme not in self.metrics:
             raise Exception("Theme no found" + theme)
 
         return list(map(lambda metric: metric.f1, self.metrics[theme]))
 
-    def get_auc(self, theme: str) -> List[float]:
 
+    def get_precision(self, theme: str) -> List[float]:
+        if theme not in self.metrics:
+            raise Exception("Theme no found" + theme)
+
+        return list(map(lambda metric: metric.precision, self.metrics[theme]))
+
+
+    def get_recall(self, theme: str) -> List[float]:
+        if theme not in self.metrics:
+            raise Exception("Theme no found" + theme)
+
+        return list(map(lambda metric: metric.recall, self.metrics[theme]))
+
+
+    def get_auc(self, theme: str) -> List[float]:
         if theme not in self.metrics:
             raise Exception("Theme no found" + theme)
 
