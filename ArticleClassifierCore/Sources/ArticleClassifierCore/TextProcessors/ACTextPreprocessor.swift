e743f8702f786143e73322d2dd5ce5135b294cf1
Jonathan Duss <jonathan.duss@iml.unibe.ch>
2021-01-03 17:24:59 +0100

    Make ACLemmatizer a protocol.
    Former-commit-id: 7d1c52d6b2c97981046918650794a37e029e66b3

--- ArticleClassifierCore/Sources/ArticleClassifierCore/TextProcessors/ACTextPreprocessor.swift
+++ ArticleClassifierCore/Sources/ArticleClassifierCore/TextProcessors/ACTextPreprocessor.swift
@@ -4,16 +4,15 @@ import NaturalLanguage
 public class ACTextPreprocessor {
     
     private let language: NLLanguage
-    private let lemmatizer: ACLemmatizer
+    public var lemmatizer: ACLemmatizer!
     
     public init(representativeText: String) {
         let languageRecognizer = NLLanguageRecognizer()
         languageRecognizer.processString(representativeText)
         language = languageRecognizer.dominantLanguage!
-        lemmatizer = ACLemmatizer(language: language)
     }
     
     public func process(text: String) -> String {
-        return lemmatizer.lemmatize(text: text, removeStopWords: true)
+        return lemmatizer.lemmatize(text: text)
     }
 }
